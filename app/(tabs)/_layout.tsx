e9fe31bd6feacb629ce6c51496f565143e39cf4c
Abhay Lonkar <abhaylonkar9@gmail.com>
2025-10-04 19:36:24 +0530

    first commit

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,44 @@
+import useTheme from "@/hooks/useTheme";
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+const TabsLayout = () => {
+    const { colors } = useTheme();
+    return (
+        <Tabs screenOptions={{
+            tabBarActiveTintColor: colors.primary,
+            tabBarInactiveTintColor: colors.textMuted,
+            tabBarStyle: {
+                backgroundColor: colors.surface,
+                borderTopWidth: 1,
+                borderTopColor: colors.border,
+                paddingTop: 10,
+                height: 90,
+                paddingBottom: 30,
+            },
+            tabBarLabelStyle: {
+                fontSize: 12,
+                fontWeight: '600',
+            },
+            headerShown: false,
+        }}>
+            <Tabs.Screen
+                name='index'
+                options={{
+                    title: "Todos",
+                    tabBarIcon: ({ color, size }) => <Ionicons name="flash-outline" size={size} color={color} />
+                }}
+            />
+            <Tabs.Screen
+                name='setting'
+                options={{
+                    title: "Settings",
+                    tabBarIcon: ({ color, size }) => <Ionicons name="settings" size={size} color={color} />
+                }}
+            />
+        </Tabs>
+    )
+}
+
+export default TabsLayout
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,27 @@
+import useTheme from "@/hooks/useTheme";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export default function Index() {
+  const { toggleDarkMode } = useTheme();
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.content}>Hello Om.</Text>
+      <TouchableOpacity onPress={toggleDarkMode}>
+        <Text>toggle dark mode</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  content: {
+    fontSize: 20,
+  }
+})
